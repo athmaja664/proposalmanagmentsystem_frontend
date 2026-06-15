f412e803778719f9b8e22625873bfa58eb227f95
athmaja664 <athmajavijayakumar@gmail.com>
2026-06-15 15:01:42 +0530

    pdf updated

--- src/pages/admin/CreateProposal.jsx
+++ src/pages/admin/CreateProposal.jsx
@@ -273,8 +273,8 @@ function CreateProposal() {
 
 
                     <div className="flex justify-end gap-3 mt-6">
-                        <button onClick={() => navigate(-1)} className="px-4 py-2 border rounded">Cancel</button>
-                        <button  onClick={handleCreate} className="px-4 py-2 bg-black text-white rounded"style={{ background: "linear-gradient(145deg, #111111 0%, #333333 100%)" }}>
+                        <button onClick={() => navigate(-1)} className="px-4 py-2 border rounded cursor-pointer">Cancel</button>
+                        <button  onClick={handleCreate} className="px-4 py-2 bg-black text-white rounded cursor-pointer"style={{ background: "linear-gradient(145deg, #111111 0%, #333333 100%)" }}>
                             Create Proposal
                         </button>
                     </div>
